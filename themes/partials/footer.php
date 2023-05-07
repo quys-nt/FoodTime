7e21ca4fef6d2911591562d41efb8a8d7e3a0b5a
nguyenquy42 <nguyenquy26012014@gmail.com>
2023-05-07 18:41:05 +0700

    template first

---
+++ themes/partials/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    footer
+</footer>
+
+<script src="<?= BASE_URL ?>/assets/vendor/js/jquery.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/js/bootstrap.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/js/slick.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/js/script.js"></script>
+</body>
+
+</html>
