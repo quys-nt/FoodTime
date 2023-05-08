cbca3b7df8ab4e43ae200c0fd1accf18f38c8ccb
nguyenquy42 <nguyenquy26012014@gmail.com>
2023-05-08 07:57:14 +0700

    html css nav

--- themes/home.php
+++ themes/home.php
@@ -1,3 +1,31 @@
 <main>
-  home
+  <section class="p-home__mv">
+    <img src="../../FoodTime/assets/imgs/img-mv-02.png" alt="img food img" class="p-home__mv--icon p-home__mv--icon01">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-7 d-flex flex-column justify-content-end position-relative">
+          <h1 class="c-text__mer c-text__h1 u-color__pri mb-4">
+            Order food online <br> from your favourite <br> local restaurants<span class="u-color__sec">.</span>
+          </h1>
+          <p class="c-text__mer mb-4">Freshly made food delivered to your door.</p>
+          <form action="#">
+            <div class="d-flex mb-5 rounded-pill p-home__form">
+              <img src="../../FoodTime/assets/imgs/icon-search-01.svg" alt="icon search 01" class="icon">
+              <input type="text" class="form-control border-0" placeholder="Enter your location">
+              <button class="btn c-btn__primary" type="submit">
+                <img src="../../FoodTime/assets/imgs/icon-search-02.svg" alt="icon search 01" class="me-2">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <div class="col-xl-5">
+          <div class="p-home__mv--img">
+            <img src="../../FoodTime/assets/imgs/img-mv-02.png" alt="img food img" class="p-home__mv--icon p-home__mv--icon02">
+            <img src="../../FoodTime/assets/imgs/img-mv-01.png" alt="img food img" class="p-home__mv--img02">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
--- themes/partials/header.php
+++ themes/partials/header.php
@@ -2,18 +2,43 @@
 <html lang="en">
 
 <head>
-    <title>FoodTime</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>FoodTime</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/vendor/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/vendor/css/slick-theme.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/vendor/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/vendor/css/slick-theme.css">
+  <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/styles.css">
+
+  <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <header>
-        header
-    </header>
+  <header class="c-header">
+    <div class="container">
+      <div class="c-header__inner">
+        <a href="/">
+          <img src="../../FoodTime/assets/imgs/logo-01.svg" alt="logo foodtime">
+        </a>
+        <div class="c-header__text">
+          <ul class="c-header__nav me-5">
+            <li class="c-header__item"><a href="#" class="c-header__link">Home</a></li>
+            <li class="c-header__item"><a href="#" class="c-header__link">Blog</a></li>
+            <li class="c-header__item"><a href="#" class="c-header__link">About us</a></li>
+          </ul>
+          <div class="c-header__list ms-5">
+            <a href="#" class="c-btn__primary">Login</a>
+            <a href="#" class="ms-3 c-btn__primary c-btn__round">
+              <img src="../../FoodTime/assets/imgs/icon-cart.svg" alt="icon cart">
+              <div class="c-btn__round--text rounded-circle">2</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
